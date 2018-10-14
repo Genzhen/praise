ff7953e90fbaeca6fab3fbe5bb6577b9b062c1d0
wang <1280210282@qq.com>
2018-10-14 15:02:50 +0800

    travis测试

---
+++ note.md
@@ -0,0 +1,2 @@
+https://testingbot.com/support/other/travis-ci  
+[解决selenium在travis中的浏览器环境问题]()  
--- test/e2e.js
+++ test/e2e.js
@@ -1,15 +1,55 @@
 
-const {Builder, By, Key, until} = require('selenium-webdriver');
+// const {Builder, By, Key, until} = require('selenium-webdriver');
 
-(async function example() {
-  let driver = await new Builder().forBrowser('firefox').build();
-  try {
-    await driver.get('http://localhost:3000/index');
-    await driver.findElement(By.id('see')).click();
-  } finally {
-    await driver.quit();
-  }
-})();
+// (async function example() {
+//   let driver = await new Builder().forBrowser('firefox').build();
+//   try {
+//     await driver.get('http://localhost:3000/index');
+//     await driver.findElement(By.id('see')).click();
+//   } finally {
+//     await driver.quit();
+//   }
+// })();
+
+
+
+var webdriver = require('selenium-webdriver'), driver;
+ 
+driver = new webdriver.Builder().
+  withCapabilities({
+    'browserName': 'firefox',
+    'platform': 'VISTA',
+    'version': 'latest',
+    'client_key': process.env.TESTINGBOT_KEY,
+    'client_secret': process.env.TESTINGBOT_SECRET,
+    'name': (process.env.TRAVIS_JOB_ID ? ("Travis Build " + process.env.TRAVIS_JOB_ID) : "Simple Test")
+  }).
+  usingServer("https://" + testingbotKey + ":" + testingbotSecret +
+              "@hub.testingbot.com/wd/hub").
+  build();
+ 
+driver.get('https://www.google.com');
+ 
+driver.getTitle().then(function (title) {
+    console.log("title is: " + title);
+});
+ 
+driver.quit();
+
+
+// import { Selector } from 'testcafe';
+
+// fixture `Getting Started`
+//     .page `http://devexpress.github.io/testcafe/example`;
+
+// test('My first test', async t => {
+//     await t
+//         .typeText('#developer-name', 'John Smith')
+//         .click('#submit-button')
+
+//         // Use the assertion to check if the actual header text is equal to the expected one
+//         .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
+// });
 
 
 
