8c57f017566871f15e3fb7b2f7862d75dbdc2895
wang <1280210282@qq.com>
2018-10-14 15:24:49 +0800

    travis测试

--- test/e2e.js
+++ test/e2e.js
@@ -1,40 +1,40 @@
 
-// const {Builder, By, Key, until} = require('selenium-webdriver');
+const {Builder, By, Key, until} = require('selenium-webdriver');
 
-// (async function example() {
-//   let driver = await new Builder().forBrowser('firefox').build();
-//   try {
-//     await driver.get('http://localhost:3000/index');
-//     await driver.findElement(By.id('see')).click();
-//   } finally {
-//     await driver.quit();
-//   }
-// })();
+(async function example() {
+  let driver = await new Builder().forBrowser('firefox').build();
+  try {
+    await driver.get('http://localhost:3000/index');
+    await driver.findElement(By.id('see')).click();
+  } finally {
+    await driver.quit();
+  }
+})();
 
 
 
-var webdriver = require('selenium-webdriver'), driver;
+// var webdriver = require('selenium-webdriver'), driver;
  
-driver = new webdriver.Builder().
-  withCapabilities({
-    'browserName': 'firefox',
-    'platform': 'VISTA',
-    'version': 'latest',
-    'client_key': process.env.TESTINGBOT_KEY,
-    'client_secret': process.env.TESTINGBOT_SECRET,
-    'name': (process.env.TRAVIS_JOB_ID ? ("Travis Build " + process.env.TRAVIS_JOB_ID) : "Simple Test")
-  }).
-  usingServer("https://" + TESTINGBOT_KEY + ":" + TESTINGBOT_SECRET +
-              "@hub.testingbot.com/wd/hub").
-  build();
+// driver = new webdriver.Builder().
+//   withCapabilities({
+//     'browserName': 'firefox',
+//     'platform': 'VISTA',
+//     'version': 'latest',
+//     'client_key': process.env.TESTINGBOT_KEY,
+//     'client_secret': process.env.TESTINGBOT_SECRET,
+//     'name': (process.env.TRAVIS_JOB_ID ? ("Travis Build " + process.env.TRAVIS_JOB_ID) : "Simple Test")
+//   }).
+//   usingServer("https://" + TESTINGBOT_KEY + ":" + TESTINGBOT_SECRET +
+//               "@hub.testingbot.com/wd/hub").
+//   build();
  
-driver.get('https://www.google.com');
+// driver.get('https://www.google.com');
  
-driver.getTitle().then(function (title) {
-    console.log("title is: " + title);
-});
+// driver.getTitle().then(function (title) {
+//     console.log("title is: " + title);
+// });
  
-driver.quit();      
+// driver.quit();      
 
 
 
